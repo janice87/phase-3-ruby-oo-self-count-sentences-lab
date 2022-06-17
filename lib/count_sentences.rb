2a5b0af8d342b837dda2454319adae16276f1fd5
Janice Chan <janice.chan1288@gmail.com>
2022-06-17 12:41:35 -0400

    commit tree fa134158932b4b4fd3d952412e546295419538b1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[\.|!|\?]/).filter { |sentence| !sentence.empty? }.count
   end
-end
+
+end 
+# \. the backslash means we want to escape and in this case we are escaping the periods usual speacial meaning. The . normally means any single character. We are saying we want only a period instead
+
+#self.split(/[\. |! |? ]*/).count
+
+# def count_sentences
+#   self.split(/[.!?]*$/)
+#   binding.pry
+#   self.count
+# end
+# end
